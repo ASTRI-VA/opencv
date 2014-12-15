cf568481a79fa0e6d821e63789d34a470c50b8c6
Joni Laitinen <jonilaitinen@astri.org>
2014-12-15 17:07:23 +0800

    Build both debug and release with build script, enable ml

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 mkdir build
 cd build
 cmake -G "Visual Studio 10 2010" -C ../BuildCacheWindows.cmake ./..
-cmake --build . --target INSTALL
+cmake --build . --target INSTALL --config Release
+cmake --build . --target INSTALL --config Debug
