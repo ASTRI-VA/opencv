d2ebe78ad722a3e2b6f1c93c7ec7825e7109c39c
Joni Laitinen <jonilaitinen@astri.org>
2015-01-09 16:39:16 +0800

    Rename windows build script

--- ASTRI/build_windows_vs10_opencv.bat
+++ ASTRI/build_windows_vs10_opencv.bat
@@ -1,6 +1,6 @@
-cv ..
-mkdir build/windows_vs10
-cd build/windows_vs10
+cd ..
+mkdir build\windows_vs10
+cd build\windows_vs10
 cmake -G "Visual Studio 10 2010" -C ../../ASTRI/BuildCacheWindows.cmake ./../..
 cmake --build . --target INSTALL --config Release
 cmake --build . --target INSTALL --config Debug
