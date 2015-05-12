76d728136c7949e9065fb5ea3122ba9b155dcd53
Joni Laitinen <jonilaitinen@astri.org>
2015-05-12 11:49:21 +0800

    Add separate build cache files for unity 4 and 5

--- ASTRI/build_ios_opencv.sh
+++ ASTRI/build_ios_opencv.sh
@@ -2,3 +2,4 @@
 
 python ../platforms/ios/build_framework.py ../build/ios_arm BuildCacheiOS.cmake
 python ../platforms/ios/build_framework.py ../build/ios_arm_unity4 BuildCacheiOSUnity4.cmake
+python ../platforms/ios/build_framework.py ../build/ios_arm_unity5 BuildCacheiOSUnity5.cmake
