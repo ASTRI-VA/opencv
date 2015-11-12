2febe9d04eac2b87421f1077ccb51db20588e4f8
Joni Laitinen <jonilaitinen@astri.org>
2015-11-12 11:42:35 +0800

    Add build cache for OSX unity 5

--- ASTRI/build_osx_opencv.sh
+++ ASTRI/build_osx_opencv.sh
@@ -3,12 +3,19 @@ mkdir -p build/osx
 cd build/osx
 
 cmake -G "Unix Makefiles" -C ../../ASTRI/BuildCacheOSX.cmake ./../..
-make -j8
+make -j16
 make install
 
 cd ..
 mkdir osx_unity
 cd osx_unity
 cmake -G "Unix Makefiles" -C ../../ASTRI/BuildCacheOSXUnity.cmake ./../..
-make -j8
+make -j16
+make install
+
+cd ..
+mkdir osx_unity5
+cd osx_unity5
+cmake -G "Unix Makefiles" -C ../../ASTRI/BuildCacheOSXUnity5.cmake ./../..
+make -j16
 make install
