29e58de9fdb0fff934776caf1dfd3516576afa7c
Joni Laitinen <jonilaitinen@astri.org>
2016-02-19 18:04:14 +0800

    Add install prefix for OS X static compile script

--- ASTRI/build_osx_static_opencv.sh
+++ ASTRI/build_osx_static_opencv.sh
@@ -2,6 +2,6 @@ cd ..
 mkdir -p build/osx_static
 cd build/osx_static
 
-cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=OFF -C ../../ASTRI/BuildCacheOSX.cmake ./../..
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=OFF -DCMAKE_INSTALL_PREFIX=install -C ../../ASTRI/BuildCacheOSX.cmake ./../..
 make -j16
 make install
